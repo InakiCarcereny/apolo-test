20d5d647634db8b2107db186ec9623eefb10cb0a
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-18 15:59:26 -0300

    Add new SVG icon components (Cross, Dot, Earth, Galaxy, Heart, HeartFill, Trash) and export them from index.ts for UI enhancement

---
+++ src/app/icons/index.ts
@@ -0,0 +1,7 @@
+export * from "./Galaxy";
+export * from "./Heart";
+export * from "./Dot";
+export * from "./Earth";
+export * from "./HeartFill";
+export * from "./Cross";
+export * from "./Trash";
